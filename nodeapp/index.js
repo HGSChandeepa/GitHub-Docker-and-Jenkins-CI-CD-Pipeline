ce8e4576d29abe57ef5e4517e818a2447a29e25e
samin <84340577+HGSChandeepa@users.noreply.github.com>
2025-01-19 21:03:00 +0530

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send('{ "response": "Hello, Welcome to Adomic Arts" }');
+  res.send('{ "response": "Hello, Welcome to Adomic Arts samin" }');
 });
 
 app.get("/will", function (req, res) {
