be466423f4cb91664a37b2b2f2192f5ad62eb7af
samin <84340577+HGSChandeepa@users.noreply.github.com>
2025-01-19 15:14:05 +0530

    Update index.js

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send('{ "response": "Hello, Welcome to Valaxy" }');
+  res.send('{ "response": "Hello, Welcome to Valaxysds" }');
 });
 
 app.get("/will", function (req, res) {
